3bb3a8db2835a751df546c4c5a4325b3b0f47bb4
Ric-gif <182771781+Ric-gif@users.noreply.github.com>
2024-11-21 10:48:50 +0200

    111111

---
+++ app.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('.btn');
+const input = document.querySelector('input');
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(input.value).then(() => {
+        button.innerText = 'Copied';
+        setTimeout(() => {
+            button.innerText = 'Copy';
+        }, 1000);
+    });
+});
